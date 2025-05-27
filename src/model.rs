7197080f461925ed2b11464eb56adac5799be727
Kyle <kylem@qd5.com>
2025-05-27 07:43:07 -0700

    adding the initial event model

---
+++ src/model.rs
@@ -0,0 +1,19 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Event {
+    pub id: Uuid,
+    pub event_type: String,
+    pub timestamp: DateTime<Utc>,
+    pub payload: Value,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct EventQuery {
+    pub event_type: Option<String>,
+    pub start: Option<DateTime<Utc>>,
+    pub end: Option<DateTime<Utc>>,
+}
